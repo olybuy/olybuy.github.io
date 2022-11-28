f71a134ff48bf0d74e212499eee2a1d83ee2d72a
Marina Kramskaia <mkhakhaeva@gmail.com>
2022-11-28 13:35:04 +0400

    Add files bear

---
+++ bear/js/menu.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".main-nav__toggle").click((function(){$(".main-nav").toggleClass("main-nav--closed")}))}));
---
+++ bear/js/modal.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".item__button, .promo-item__button").click((function(){$(".modal").removeClass("modal--closed")})),$(".modal__toggle").click((function(){$(".modal").addClass("modal--closed")}))}));
