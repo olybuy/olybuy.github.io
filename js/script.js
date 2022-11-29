7e3a99e2165a7479202144f5b598f95c4e22f9a2
Marina Kramskaia <mkhakhaeva@gmail.com>
2022-11-29 15:53:03 +0400

    Add script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let sedona = document.querySelector('.button-sedona');
 let sedonaScss = document.querySelector('.button-sedona-scss');
 let bear = document.querySelector('.button-bear');
-let buttonsArr = document.querySelectorAll('.projects__link');
 
 sedona.onmouseover = function () {
     document.querySelector('.note-sedona').classList.add('display');
@@ -11,9 +10,18 @@ sedona.onmouseout = function () {
     document.querySelector('.note-sedona').classList.remove('display');
 }
 
-document.onmouseover = function (event) {
-    let target = event.target;
-    if (target.classList.contains('projects__link')) {
-        console.log(target.classList);
-    }
+sedonaScss.onmouseover = function () {
+    document.querySelector('.note-sedona-scss').classList.add('display');
+}
+
+sedonaScss.onmouseout = function () {
+    document.querySelector('.note-sedona-scss').classList.remove('display');
+}
+
+bear.onmouseover = function () {
+    document.querySelector('.note-bear').classList.add('display');
+}
+
+bear.onmouseout = function () {
+    document.querySelector('.note-bear').classList.remove('display');
 }
