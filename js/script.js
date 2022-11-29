bb88aea9629d0e591df553babea65623a5eea6b7
Marina Kramskaia <mkhakhaeva@gmail.com>
2022-11-29 14:29:53 +0400

    Add script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let projects = document.querySelectorAll('.projects__link');
 let notes = document.querySelectorAll('.notes__item');
 
 document.addEventListener('mouseover', function (event) {
+    console.log(event.target);
     if (event.target.classList.contains('sedona1')) {
         console.log('sedona1');
     }
