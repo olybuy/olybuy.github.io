c9923bffbfb61bb8a9a5c3512b1c4e213fe1b132
Marina Kramskaia <mkhakhaeva@gmail.com>
2022-11-29 14:07:25 +0400

    Add script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let projects = document.querySelectorAll('.projects__link');
+let notes = document.querySelectorAll('.notes__item');
+
+ul.onclick = function (event) {
+    let target = event.target;
+    console.log(target);
+}
