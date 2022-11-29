0aad54fbd53fd88f6697f85723829bfb1f8e5338
Marina Kramskaia <mkhakhaeva@gmail.com>
2022-11-29 14:15:06 +0400

    Add script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let projects = document.querySelectorAll('.projects__link');
 let notes = document.querySelectorAll('.notes__item');
 
-ul.onclick = function (event) {
-    let target = event.target;
-    console.log(target);
-}
+document.addEventListener('click', function (event) {
+    console.log(event.target);
+})
