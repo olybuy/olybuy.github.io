d6d81663f792310edf6c625007b2ee2423704b3f
Marina Kramskaia <mkhakhaeva@gmail.com>
2023-01-11 14:58:54 +0400

    Slider

--- js/script.js
+++ js/script.js
@@ -3,6 +3,45 @@ let sedonaScss = document.querySelector('.button-sedona-scss');
 let bear = document.querySelector('.button-bear');
 let housevop = document.querySelector('.button-housevop');
 
+
+let slider = document.querySelector('.slider');
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+let slides = document.querySelectorAll('.projects__item');
+let slideList = document.querySelector('.projects__list');
+let slideIndex = 0;
+let screenWidth = document.documentElement.clientWidth;
+
+
+slide = function () {
+    let n = -170;
+    slideList.style.transition = 'transform .5s';
+    slideList.style.transform = 'translateX(' + n * slideIndex  + 'px)';
+
+    arrowLeft.classList.toggle('disabled', slideIndex === 0);
+    if (screenWidth < 480) {
+        arrowRight.classList.toggle('disabled', slideIndex === --slides.length);
+    } else if (screenWidth >= 480 && screenWidth < 650) {
+        arrowRight.classList.toggle('disabled', slideIndex === --slides.length-1);
+    } else {
+        arrowRight.classList.toggle('disabled', slideIndex === --slides.length-2);
+    }
+}
+
+slider.addEventListener('click', function () {
+    let target = event.target;
+
+    if (target === arrowRight) {
+        slideIndex++;
+    } else if (target === arrowLeft) {
+        slideIndex--
+    } else {
+        return;
+    }
+
+    slide();
+})
+
 let display = function (elemClass) {
     document.querySelector(elemClass).classList.add('display');
 }
