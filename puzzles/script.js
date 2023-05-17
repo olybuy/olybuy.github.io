451077caade2157ef6ee2f836ad089b412bb93c6
Marina Kramskaia <mkhakhaeva@gmail.com>
2023-05-17 22:52:48 +0400

    Fix touch

--- puzzles/script.js
+++ puzzles/script.js
@@ -1,45 +1,39 @@
-container.onmousedown = function (event) {
+container.onpointerdown = function (event) {
     let target = event.target;
 
-    target.style.position = 'absolute';
-    target.style.zIndex = 1000;
-    document.body.append(target);
-    moveAt(event.pageX, event.pageY);
+    if (target.classList.contains('image')) { // если клик был на картинке (в выборе или на поле)
+        target.style.position = 'absolute';
+        target.style.zIndex = 1000;
+        document.body.append(target);
+        moveAt(target, event.pageX, event.pageY);
 
-    function moveAt(pageX, pageY) {
-        target.style.left = pageX - target.offsetWidth / 2 + 'px';
-        target.style.top = pageY - target.offsetHeight / 2 + 'px';
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    target.onmouseup = function(event) {
-        document.removeEventListener('mousemove', onMouseMove);
-        target.onmouseup = null;
-        target.style.width = '150px';
-        target.style.height = '150px';
-
-        target.hidden = true;
-
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-        if (elemBelow.classList.contains('element-cell')) {
-            target.style.cssText = 'none';
-            elemBelow.append(target);
-            target.hidden = false;
+        function onPointerMove(event) {
+            moveAt(target, event.pageX, event.pageY);
         }
 
-        if (elemBelow.classList.contains('image-part')) {
-            target.style.cssText = 'none';
-            elemBelow.append(target);
-            target.hidden = false;
-        }
-    };
+        document.addEventListener('pointermove', onPointerMove);
 
-    target.ondragstart = function() {
-        return false;
-    };
+        target.onpointerup = function(event) {
+            document.removeEventListener('pointermove', onPointerMove);
+            target.onpointerup = null;
+            target.style.width = '150px';
+            target.style.height = '150px';
+
+            target.hidden = true;
+            let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            if (elemBelow.classList.contains('element-cell') || elemBelow.classList.contains('image-part')) {
+                target.style.cssText = 'none';
+                elemBelow.append(target);
+                target.hidden = false;
+            }
+        };
+        target.ondragstart = function() {
+            return false;
+        };
+    }
+}
+
+function moveAt(target, pageX, pageY) {
+    target.style.left = pageX - target.offsetWidth / 2 + 'px';
+    target.style.top = pageY - target.offsetHeight / 2 + 'px';
 }
