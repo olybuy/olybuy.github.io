6b9e189afde650c7e0e1cd9e9ce4add8412cc6ce
Marina Kramskaia <mkhakhaeva@gmail.com>
2023-05-18 11:35:55 +0400

    Fix return to elements

--- puzzles/script.js
+++ puzzles/script.js
@@ -1,14 +1,17 @@
 container.onpointerdown = function (event) {
-    let target = event.target;
+    const target = event.target;
+    target.hidden = true;
+    const elemBelowTarget = document.elementFromPoint(event.clientX, event.clientY);
+    target.hidden = false;
 
-    if (target.classList.contains('image')) { // если клик был на картинке (в выборе или на поле)
+    if (target.classList.contains('image')) {
         target.style.position = 'absolute';
         target.style.zIndex = 1000;
         document.body.append(target);
-        moveAt(target, event.pageX, event.pageY);
+        moveAt(event.pageX, event.pageY);
 
         function onPointerMove(event) {
-            moveAt(target, event.pageX, event.pageY);
+            moveAt(event.pageX, event.pageY);
         }
 
         document.addEventListener('pointermove', onPointerMove);
@@ -16,24 +19,39 @@ container.onpointerdown = function (event) {
         target.onpointerup = function(event) {
             document.removeEventListener('pointermove', onPointerMove);
             target.onpointerup = null;
-            target.style.width = '150px';
-            target.style.height = '150px';
 
             target.hidden = true;
-            let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-            if (elemBelow.classList.contains('element-cell') || elemBelow.classList.contains('image-part')) {
-                target.style.cssText = 'none';
-                elemBelow.append(target);
-                target.hidden = false;
+            const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+
+            if (elemBelow.classList.contains('element-cell')) {
+                target.style.width = '150px';
+                target.style.height = '150px';
+                placeImage(target, elemBelow);
+                elemBelowTarget.remove();
+            } else if (elemBelow.classList.contains('image')) {
+                const placeForImage = document.createElement('div');
+                placeForImage.classList.add('image-part');
+                placeImage(target, placeForImage);
+                const elem = elemBelow.parentNode;
+                elem.after(placeForImage);
+            } else {
+                placeImage(target, elemBelowTarget);
             }
         };
+
+        function moveAt(pageX, pageY) {
+            target.style.left = pageX - target.offsetWidth / 2 + 'px';
+            target.style.top = pageY - target.offsetHeight / 2 + 'px';
+        }
+
+        function placeImage(image, place) {
+            image.style.cssText = 'none';
+            place.append(image);
+            image.hidden = false;
+        }
+
         target.ondragstart = function() {
             return false;
         };
     }
-}
-
-function moveAt(target, pageX, pageY) {
-    target.style.left = pageX - target.offsetWidth / 2 + 'px';
-    target.style.top = pageY - target.offsetHeight / 2 + 'px';
 }
